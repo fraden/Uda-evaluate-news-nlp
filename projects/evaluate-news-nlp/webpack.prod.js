1afa054a57aa48a772dfdff294737fde44799726
Dennis Frankenbach <dennisfrankenbach@gmail.com>
2021-10-21 23:00:56 +0200

    setup webpack for production - according to lesson 4 - 5

--- projects/evaluate-news-nlp/webpack.prod.js
+++ projects/evaluate-news-nlp/webpack.prod.js
@@ -1,10 +1,20 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin"); // instead of "optimize-css-assets-webpack-plugin" according to https://www.npmjs.com/package/optimize-css-assets-webpack-plugin
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     mode: 'production',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new CssMinimizerPlugin({})],
+    },
     module: {
         rules: [{
                 test: '/\.js$/',
@@ -13,14 +23,15 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            }
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
         ]
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" })
     ]
 }
