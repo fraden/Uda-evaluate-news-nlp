6772e3934ff786d893f36558e5bcd4a1b2899b84
Dennis Frankenbach <dennisfrankenbach@gmail.com>
2021-10-22 16:33:01 +0200

    refactor: move code to formHandler.js

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,16 +1,46 @@
-function handleSubmit(event) {
+async function handleSubmit(event) {
     event.preventDefault()
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+    let agreementText = document.getElementById("agreement");
+    let subjectivityText = document.getElementById("subjectivity");
+    let confidenceText = document.getElementById("confidence");
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/api')
-        .then(res => res.json())
-        .then(function(res) {
-            document.getElementById('results').innerHTML = res.message
-        })
+    // check what text was put into the form field
+    let urlText = document.getElementById('url').value
+    Client.checkForName(urlText)
+
+    const postData = async(url = '', data = {}) => {
+        // Default options are marked with *
+        const res = await fetch(url, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ text: data })
+        });
+
+        try {
+            const newData = await res.json();
+            return newData;
+        } catch (error) {
+            console.log("error", error);
+        }
+    }
+
+    let sentiment = await postData('http://localhost:8081/api', urlText)
+        .then(sentiment => sentiment.json())
+        .then(
+            function(res) {
+                console.log("----")
+                console.log(res)
+                agreementText.innerHTML = `Agreement: ${res.agreement}`;
+                subjectivityText.innerHTML = `Subjectivity: ${res.subjectivity}`;
+                confidenceText.innerHTML = `Confidence: ${res.confidence}`;
+            }
+        )
+
+
 }
 
 export { handleSubmit }
