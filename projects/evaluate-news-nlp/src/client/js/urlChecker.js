83f40554238445199d2c9e9797f000da2ebd4510
Dennis Frankenbach <dennisfrankenbach@gmail.com>
2021-10-23 22:31:06 +0200

    feat: urlChecker added + sum tests

---
+++ projects/evaluate-news-nlp/src/client/js/urlChecker.js
@@ -0,0 +1,6 @@
+const urlChecker = (url) => {
+    let regex = /^(ftp|http|https):\/\/[^ "]+$/; // source: https://stackoverflow.com/questions/1410311/regular-expression-for-url-validation-in-javascript
+    return regex.test(url);
+}
+
+module.exports = urlChecker;
---
+++ projects/evaluate-news-nlp/src/client/js/urlChecker.test.js
@@ -0,0 +1,9 @@
+const urlChecker = require('./urlChecker');
+
+test('checks valid url', () => {
+    expect(urlChecker("http://google.com/")).toBe(true);
+});
+
+test('checks wrong url', () => {
+    expect(urlChecker("google.com/")).toBe(false);
+});
